2b70a57efa80497de5fa230f7d849f100f37df98
Péter Kalicz <kalicz.peter@uni-sopron.hu>
2020-08-12 06:48:44 +0200

    Select toppings

--- js/main.js
+++ js/main.js
@@ -28,3 +28,21 @@ let alertCloseEventHandlerFunction = function(ev){
 for (let i = 0; i < alertCloseButtons.length; i++) {
     alertCloseButtons[i].addEventListener("click", alertCloseEventHandlerFunction);
 }
+
+// Select elem kitöltése
+let toppings = [
+    "Szalonna",
+    "Hagyma",
+    "Tükörtojás",
+    "Libamáj",
+    "Extra sonka"
+];
+let toppingSelect = document.querySelector("#topInput");
+let index = 0;
+while(index < toppings.length) {
+    let option = document.createElement("option");
+    option.value = toppings[index];
+    option.innerHTML = toppings[index];
+    toppingSelect.appendChild(option);
+    index++;
+}
