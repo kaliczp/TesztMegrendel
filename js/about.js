9c47df73d6a542f6f3a20d4243882d3157b9ea59
Péter Kalicz <kalicz.peter@uni-sopron.hu>
2020-08-12 07:16:29 +0200

    New javascript with users data

---
+++ js/about.js
@@ -0,0 +1,10 @@
+let users = [
+    {surname: "Berger", firstname: "Whitney", age: 22},
+    {surname: "Nagy", firstname: "Árpád", age: 44},
+    {surname: "Kiss", firstname: "Bence", age: 33},
+    {surname: "Doe", firstname: "John", age: 42},
+    {surname: "Vadölő", firstname: "Jack", age: 32},
+    {surname: "Rostás", firstname: "Márió", age: 45},
+    {surname: "Fehér", firstname: "Péter", age: 54},
+    {surname: "Piros", firstname: "Gizella", age: 12}
+];
