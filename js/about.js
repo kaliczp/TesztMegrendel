e3f0dab4e04233afc55e8424fbbd1b6f40e8283d
Péter Kalicz <kalicz.peter@uni-sopron.hu>
2020-08-12 07:24:23 +0200

    Users number added

--- js/about.js
+++ js/about.js
@@ -1,3 +1,22 @@
+/*
+<tr>
+                    <td>1</td>
+                    <td>Kiss</td>
+                    <td>János</td>
+                    <td>55</td>
+                    <td>
+                        <div class="btn-group">
+                            <button class="btn-info btn">
+                                módosítás
+                            </button>
+                            <button class="btn btn-danger">
+                                törlés
+                            </button>
+                        
+                        </div>
+                    </td>
+                </tr>
+                */
 let users = [
     {surname: "Berger", firstname: "Whitney", age: 22},
     {surname: "Nagy", firstname: "Árpád", age: 44},
@@ -8,3 +27,11 @@ let users = [
     {surname: "Fehér", firstname: "Péter", age: 54},
     {surname: "Piros", firstname: "Gizella", age: 12}
 ];
+let tableBody = document.querySelector("#userTable tbody")
+for(let k in users) {
+    let tr = document.createElement("tr");
+    let td = document.createElement("td");
+    td.innerHTML = parseInt(k)+1;
+    tr.appendChild(td);
+    tableBody.appendChild(tr);
+}
